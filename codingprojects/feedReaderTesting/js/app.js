6342d2fa8ebb1b178fd934d8940b64ad74ddb91d
KendrickPC <kennethpchang@gmail.com>
2018-02-17 22:55:34 +0800

    attempted to fix error timeout async callback issue

--- codingprojects/feedReaderTesting/js/app.js
+++ codingprojects/feedReaderTesting/js/app.js
@@ -77,6 +77,7 @@ function loadFeed(id, cb) {
 /* Google API: Loads the Feed Reader API and defines what function
  * to call when the Feed Reader API is done loading.
  */
+google.load('feeds', '1');
 google.setOnLoadCallback(init);
 /* All of this functionality is heavily reliant upon the DOM, so we
  * place our code in the $() function to ensure it doesn't execute
